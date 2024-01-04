cc8ed773b6d43b305fb20ba172e373ce468dc03d
dinesh6705 <dinesh.sharma@adglobal360.com>
2024-01-04 10:26:22 +0530

    space

--- App.js
+++ App.js
@@ -7,7 +7,6 @@ import { PersistGate } from 'redux-persist/integration/react';
 export default function App() {
   return (
 
-
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <StackNavigation />
